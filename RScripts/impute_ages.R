3eaa21c44020cab8923082b37b04fcbb5b66c853
Crystal Shaw <c.shaw@ucla.edu>
2020-05-03 21:39:52 -0700

    function to impute ages based on wave O age

---
+++ RScripts/impute_ages.R
@@ -0,0 +1,17 @@
+impute_ages <- function(vector){
+  #No one in our analytic sample has a vector of ages completely missing 
+  #because everyone has age at Wave O
+  last_age <- length(vector)
+  missing_age <- which(vector == 999)
+  
+  for(slot in missing_age){
+    vector[slot] <- vector[last_age] - 2*(last_age - slot)
+  }
+  
+  return(vector)
+  
+}
+
+# #test vectors
+# vector <- c(999, 75, 77, 79, 81)
+# vector <- c(75, 999, 999, 81, 83)

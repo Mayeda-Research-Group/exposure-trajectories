c699ae50bdd54e5bd4d1d552708b258663a0174a
Crystal Shaw <c.shaw@ucla.edu>
2020-05-03 22:00:01 -0700

    Base follow-up time on ages at waves instead of time between waves

--- RScripts/fu_time.R
+++ RScripts/fu_time.R
@@ -1,16 +1,19 @@
 fu_time <- function(vector){
-  idx <- !is.na(vector) 
+  waves <- vector[1:5]
+  ages <- vector[6:10]
+  idx <- !is.na(waves) 
   
   if(any(idx)){
     first_slot <- min(which(idx))
     last_slot <- max(which(idx))
     
-    return(2*(last_slot - first_slot))
+    return(ages[last_slot] - ages[first_slot])
   } else{
     return(NA)
   }
 }
 
-# #Test vectors
-# vector <- c(NA, 1, NA, 2, NA, NA)
-# vector <- rep(NA, 5)
+# #Test df
+# test <- analytic_df %>% 
+#   dplyr::select(contains(c("CYSC_ADJ", "AGE")))
+# vector <- test[1, ]

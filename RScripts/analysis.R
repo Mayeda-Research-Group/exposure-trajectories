53eb5d021613f1f4dda508b6f9057f9a09268bad
Crystal Shaw <c.shaw@ucla.edu>
2020-08-09 16:43:31 -0700

    Initial commit of analysis script

---
+++ RScripts/analysis.R
@@ -0,0 +1,17 @@
+#---- Package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("here", "tidyverse")
+
+#No scientific notation
+options(scipen = 999)
+
+#---- Read in analytical sample ----
+analytical_sample <- read_csv(paste0("/Users/CrystalShaw/Dropbox/Projects/", 
+                                     "exposure_trajectories/data/", 
+                                     "hrs_samp_alive_70_cysc_60_70.csv"))
+
+#---- Missing data in predictors ----
+#Predictors of Cystatin C: 

f1130b7451c178e9ca9de23f3a4063a33d052791
Crystal Shaw <c.shaw@ucla.edu>
2020-08-12 14:40:50 -0700

    Set data types when reading in imputation data Create imputation location indicator matrix Create predictor matrix

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -9,21 +9,52 @@ p_load("here", "tidyverse", "mice")
 options(scipen = 999)
 
 #---- Read in analytical sample ----
-imputation_data <- read_csv(paste0("/Users/CrystalShaw/Dropbox/Projects/", 
-                                   "exposure_trajectories/data/", 
-                                   "imputation_data.csv"), 
-                            col_types = cols(.default = col_double(), 
-                                             HHIDPN = col_character()))
+imputation_data <- 
+  read_csv(paste0("/Users/CrystalShaw/Dropbox/Projects/", 
+                  "exposure_trajectories/data/", 
+                  "imputation_data.csv"), 
+           col_types = cols(.default = col_double(), HHIDPN = col_character(), 
+                            death = col_factor(), female = col_factor(), 
+                            hispanic = col_factor(), black = col_factor(), 
+                            other = col_factor()))
 
 #---- Missing data in predictors ----
 #Predictors of Cystatin C: 
 # baseline: Sex/gender, race/ethnicity, cSES, death, age at death
 # time-varying: 
 
-# #Looking at what else needs to be imputed-- no other missing data at this time
-# colSums(is.na(imputation_data))
+#Define where we want data imputed
+impute_here <- is.na(imputation_data) %>% 
+  set_colnames(colnames(imputation_data))*1
 
+#Check where there is missing data
+colSums(impute_here)
 
+impute_here[, c("age_death_y", paste0("logCYSC_ADJ_", seq(70, 78)))] <- 0
+
+#---- predictor matrix ----
+predictors <- matrix(1, ncol = ncol(imputation_data), 
+                     nrow = ncol(imputation_data)) %>% 
+  set_colnames(colnames(imputation_data)) %>% 
+  set_rownames(colnames(imputation_data))
+diag(predictors) <- 0
+
+predictors[, "HHIDPN"] <- 0
+
+#---- MICE ----
+# #Look at missing data pattern
+# md.pattern(imputation_data %>% dplyr::select(contains("CYSC")))
+
+#Imputation
+#Want 25 imputations 
+#maxit seems to be the number of iterations for the trace plot
+imputations <- mice(imputation_data, m = 2, maxit = 25, where = impute_here,
+                    defaultMethod = rep("pmm", 4), seed = 20200812)
+
+#check diagnostics
+imputations$loggedEvents
+plot(imputations)
+#densityplot(imp_pmm_all_brfss, ~income)
 
 
 

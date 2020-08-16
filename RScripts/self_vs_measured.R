3241050ffb5e998f0deed4932bcc3df3628d9fe1
Crystal Shaw <c.shaw@ucla.edu>
2020-08-15 21:00:53 -0700

    function to format self-report vs. measured data

--- RScripts/self_vs_measured.R
+++ RScripts/self_vs_measured.R
@@ -4,20 +4,36 @@
 #when available. This function also creates a set of columns [measure]_measured
 #which indicates that the value is measured (vs. self-reported)
 
-#Get measured BMI
-BMI <- cbind(hrs_samp[, paste0("r", number_waves, "pmbmi")], 
-             1 - is.na(hrs_samp[, paste0("r", number_waves, "pmbmi")]))
-#Set NAs to 0 so we can pick up remaining NAs from self-report
-BMI[is.na(BMI)] <- 0 
-colnames(BMI) <- c(paste0(letter_waves, "BMI"), 
-                   paste0(letter_waves, "BMI_measured"))
+measured_self_report <- function(data, measured_cols, self_cols, 
+                                 derived_variable){
+  #Get measured values
+  derived <- cbind(data[, measured_cols], 
+                   1 - is.na(data[, measured_cols]))
+  #Set NAs to 0 so we can pick up remaining NAs from self-report
+  derived[is.na(derived)] <- 0 
+  colnames(derived) <- c(paste0(letter_waves, derived_variable), 
+                         paste0(letter_waves, derived_variable, "_measured"))
+  
+  #Pick up self-reported measure for empty (= 0) cells
+  pick_up <- (derived[, paste0(letter_waves, derived_variable)] == 0)*1*
+    data[, self_cols]
+  
+  #Fill in self-reported data
+  derived[, paste0(letter_waves, derived_variable)] <- 
+    derived[, paste0(letter_waves, derived_variable)] + pick_up
+  
+  #---- return values ----
+  return(derived)
+}
 
-#Pick up self-reported BMI for empty (= 0) cells
-pick_up <- (BMI[, paste0(letter_waves, "BMI")] == 0)*1*
-  hrs_samp[, paste0("r", number_waves, "bmi")]
+# #---- unit testing ----
+# test <- measured_self_report(hrs_samp, paste0("r", number_waves, "pmbmi"), 
+#                              paste0("r", number_waves, "bmi"), "BMI")
+# 
+# colnames(test) == colnames(BMI)
+# sum(is.na(test)) == sum(is.na(BMI))
+# nrow(test)*ncol(test) == sum(is.na(test)) + 
+#   sum((test == BMI) == TRUE, na.rm = TRUE)
 
-#Fill in self-reported BMI
-BMI[, paste0(letter_waves, "BMI")] <- 
-  BMI[, paste0(letter_waves, "BMI")] + pick_up
 
-hrs_samp %<>% cbind(BMI)
+

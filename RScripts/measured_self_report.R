7b62f60fb6a12ef36410bd5aee05039dc2442d1b
Crystal Shaw <c.shaw@ucla.edu>
2020-09-09 14:48:03 -0700

    Fixed errors of NAs wiping out measured values-- yikes!

--- RScripts/measured_self_report.R
+++ RScripts/measured_self_report.R
@@ -10,17 +10,25 @@ measured_self_report <- function(data, measured_cols, self_cols,
   derived <- cbind(data[, measured_cols], 
                    1 - is.na(data[, measured_cols]))
   #Set NAs to 0 so we can pick up remaining NAs from self-report
+  #NAs in self-reported data will be coded at -1 at first b/c I had a problem
+  #with NA-ing out existing values otherwise
   derived[is.na(derived)] <- 0 
   colnames(derived) <- c(paste0(letter_waves, derived_variable), 
                          paste0(letter_waves, derived_variable, "_measured"))
   
+  self_report <- data[, self_cols]
+  self_report[is.na(self_report)] <- -1
+  
   #Pick up self-reported measure for empty (= 0) cells
   pick_up <- (derived[, paste0(letter_waves, derived_variable)] == 0)*1*
-    data[, self_cols]
+    self_report[, ]
   
   #Fill in self-reported data
   derived[, paste0(letter_waves, derived_variable)] <- 
     derived[, paste0(letter_waves, derived_variable)] + pick_up
+  
+  #Turn -1 to NA
+  derived[derived < 0] <- NA
   
   #---- return values ----
   return(derived)
@@ -34,6 +42,12 @@ measured_self_report <- function(data, measured_cols, self_cols,
 # sum(is.na(test)) == sum(is.na(BMI))
 # nrow(test)*ncol(test) == sum(is.na(test)) + 
 #   sum((test == BMI) == TRUE, na.rm = TRUE)
+
+# data = hrs_samp
+# measured_cols = paste0("r", number_waves, "pmbmi")
+# self_cols = paste0("r", number_waves, "bmi")
+# derived_variable = "BMI"
+
 
 
 

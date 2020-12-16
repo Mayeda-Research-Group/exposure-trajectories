e4ef9b89187cd47f115fd6acc15e0b345b75380c
Crystal Shaw <c.shaw@ucla.edu>
2020-12-16 13:45:10 -0800

    make long dataset for imputation

--- RScripts/induce_missing_MI.R
+++ RScripts/induce_missing_MI.R
@@ -50,9 +50,16 @@ CESD_subset <- CESD_data_wide %>% dplyr::select(all_of(CESD_vars))
 #---- Check missingness ----
 colSums(is.na(CESD_subset))
 
+#---- make long dataset ----
+CESD_only_long <- CESD_data_wide %>% 
+  dplyr::select("HHIDPN", paste0("r", seq(4, 9), "cesd")) %>% 
+  pivot_longer(contains("cesd"), values_to = "cesd", names_to = "wave")
+
 #---- induce missingness ----
-CESD_only <- CESD_data_wide %>% 
-  dplyr::select("HHIDPN", paste0("r", seq(4, 9), "cesd"))
+mcar10 <- sample(obs_in_range, size = floor(0.10*length(obs_in_range)))
+
+imputation_data_long[, "mcar10"] <- 0
+imputation_data_long[mcar10, "mcar10"] <- 1
 
 mcar10 <- CESD_only
 for(i in 2:ncol(mcar10)){
@@ -66,7 +73,110 @@ for(i in 2:ncol(mcar10)){
 # floor(0.10*nrow(mcar10)*(ncol(mcar10) - 1))
 # table(rowSums(is.na(mcar10)))
 
+
+
+# #Sanity check
+# imputation_data_long %>% filter(age_y_int >= 70) %>% summarise_at("mcar10", sum)
+# sum(imputation_data_long$mcar10)
+
+#mask values based on missing value indicator
+imputation_data_long %<>% 
+  mutate("log_CysC_masked" = ifelse(mcar10 == 1, NA, log_CysC)) %>%
+  mutate("CysC_masked" = exp(log_CysC_masked))
+
+# #Sanity check
+# View(imputation_data_long[, c("age_y_int", "log_CysC", "log_CysC_masked",
+#                               "mcar10")])
+
+#---- Remove people with no Cystatin C measures ----
+#On this run, I've removed 78 people
+no_cysc <- imputation_data_long %>% group_by(HHIDPN) %>% 
+  summarise_at("log_CysC_masked", function(x) sum(!is.na(x))) %>% 
+  filter(log_CysC_masked == 0)
+
+imputation_data_long %<>% filter(!HHIDPN %in% no_cysc$HHIDPN)
+
+#---- check col types of dataframe ----
+sapply(imputation_data_long, class)
+
+imputation_data_long %<>% 
+  mutate_at(c("observed", "mcar10"), as.factor)
+
+#---- Specify formulas ----
+meth <- make.method(imputation_data_long)
+meth["BMI"] <- "~I(weight / height^2)"
+meth["CysC_masked"] <- "~I(exp(log_CysC_masked))"
+
+#---- predictor matrix ----
+pred <- make.predictorMatrix(imputation_data_long)
+
+#Don't use these as predictors
+pred[, c("HHIDPN", "log_CysC", "observed", "mcar10", "height_measured", 
+         "Wave", "weight_measured", "BMI_measured", "CYSC_ADJ", 
+         "CysC_masked")] <- 0
+
+#Formulas are already specified for these
+pred[c("BMI", "CysC_masked"), ] <- 0
+
+#Do not need imputations for these-- do I even need to specify this?
+pred[c("HHIDPN", "female", "hispanic", "black", "other", "raedyrs", "death", 
+       "height", "height_measured", "Wave", "age_y_int", "weight_measured", 
+       "BMI_measured", "CYSC_ADJ", "log_CysC", "observed", "mcar10"), ] <- 0
+
+#---- Missing data in predictors ----
+#Predictors of Cystatin C: 
+# baseline: Sex/gender, race/ethnicity, cSES, death, age at death, 
+#           smoking status
+# time-varying: 
+
+#Indicate where there is missing data in the long data
+impute_here_long <- is.na(imputation_data_long) %>% 
+  set_colnames(colnames(imputation_data_long))*1
+
+missingness <- t(colSums(impute_here_long)/nrow(impute_here_long)) %>% 
+  as.data.frame() %>% round(., 2) 
+
+#Indicate where we don't want imputations-- original data
+impute_here_long[, c("CYSC_ADJ", "log_CysC")] <- 0
+
+missingness_table <- missingness %>%
+  dplyr::select(-c("HHIDPN", "height_measured", "Wave", "weight_measured", 
+                   "BMI_measured", "observed", "log_CysC", "mcar10", 
+                   "log_CysC_masked", "CysC_masked"))
+
+write_csv(missingness_table, 
+          paste0("/Users/CrystalShaw/Dropbox/Projects/", 
+                 "exposure_trajectories/manuscript/", 
+                 "tables/missingness.csv"))
+
+#---- MICE ----
+# #Look at missing data pattern
+# md.pattern(imputation_data %>% dplyr::select(contains("CYSC")))
+
+#FCS Imputation
+#Want 25 imputations 
+#maxit seems to be the number of iterations for the trace plot
+num_impute = 3
+imputations <- mice(imputation_data_long, m = num_impute, maxit = 5, 
+                    predictorMatrix = pred, 
+                    where = impute_here_long,
+                    defaultMethod = rep("norm", 4), seed = 20200812)
+
+# #check diagnostics
+# View(imputations$loggedEvents)
+# plot(imputations)
+# densityplot(imputations, ~ age_death_y)
+
+# #Checking
+# sample_original <- complete(imputations, action = 0)
+# sample_complete <- complete(imputations, action = 3)
+# 
+# colSums(is.na(sample_original))
+# colSums(is.na(sample_complete))
+
+
 #---- MI ----
+
 
 #---- imputed exposure defs ----
 

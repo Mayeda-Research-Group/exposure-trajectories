5cd4ded31fef249ccfdca336c7cab453496fb5d6
yw-mori11 <wuyingyan1120@gmail.com>
2021-02-01 10:24:52 -0800

    heatmap for KAPPA value not stratified by occasions

--- RScripts/heatmap_CESD_KAPPA.R
+++ RScripts/heatmap_CESD_KAPPA.R
@@ -28,18 +28,44 @@ path_to_dropbox <- "C:/Users/yingyan_wu/Dropbox"
 CESD_data_wide <- read_csv(paste0(path_to_dropbox,
                                   "/exposure_trajectories/data/",
                                   "CESD_data_wide.csv"))
+table(CESD_data_wide$total_elevated_cesd)
 
 #---- KAPPA test ----
 
-fmsb::Kappa.test(
+kappa_4_9 <- fmsb::Kappa.test(
   table(CESD_data_wide$r4cesd_elevated, CESD_data_wide$r9cesd_elevated))$
   Result$estimate
 
-fmsb::Kappa.test(
+kappa_4_avg <- fmsb::Kappa.test(
   table(CESD_data_wide$r4cesd_elevated, CESD_data_wide$avg_cesd_elevated))$
   Result$estimate
 
-fmsb::Kappa.test(
+kappa_9_avg <- fmsb::Kappa.test(
   table(CESD_data_wide$r9cesd_elevated, CESD_data_wide$avg_cesd_elevated))$
   Result$estimate
+
+#---- KAPPA matrix ----
+kappa_value <- c(NA, kappa_4_9,kappa_4_avg, 
+                kappa_4_9, NA, kappa_9_avg,
+                kappa_4_avg, kappa_9_avg, NA)
+rowname <- c("r4cesd_elevated", "r9cesd_elevated", "avg_cesd_elevated")
+colname <- c("r4cesd_elevated", "r9cesd_elevated", "avg_cesd_elevated")
+
+KAPPA_mat <- matrix(kappa_value, nrow = 3, ncol = 3, dimnames = list(rowname, colname))
+# heatmap(KAPPA_mat)
+
+KAPPA_dat <- reshape2::melt(KAPPA_mat)
+
+#---- heat map ----
+ggplot(data = KAPPA_dat,
+       aes(x = Var1, y = Var2, fill = value)) +
+  theme_minimal()+
+  geom_tile(color = "white") + 
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white",
+                       midpoint = 0.6, limit = c(0,1))+
+  geom_text(aes(Var2, Var1, label = round(value,2)), color = "black", size = 6)+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
+
 

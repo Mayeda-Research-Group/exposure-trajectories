c5d0827e4ec8ac351fe54f2abe067525e94e36c7
Crystal Shaw <c.shaw@ucla.edu>
2021-02-01 12:49:34 -0800

    transform data in all mcar datasets use BMI, Age, and shlt at wave of imputation run imputations in a loop store data for imputation viz

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -167,9 +167,14 @@ write_csv(missings, path = paste0(path_to_dropbox,
                                   "tables/missing_counts.csv"))
 
 #---- transformations ----
-for(wave in 4:9){
-  mcar10[, paste0("logr", wave, "cesd")] <- 
-    log(1 + mcar10[, paste0("r", wave, "cesd")])
+#Taking the log for joint MNV models
+for(prop in mask_props){
+  data <- get(paste0("mcar", 100*prop))
+  for(wave in 4:9){
+    data[, paste0("logr", wave, "cesd")] <- 
+      log(1 + data[, paste0("r", wave, "cesd")])
+  }
+  assign(paste0("mcar", 100*prop), data)
 }
 
 #---- imputation ----
@@ -178,6 +183,7 @@ num_impute <- 5
 #---- **JMVN ----
 #Joint multivariate normal
 #---- ***predictor matrix ----
+#The predictor matrix is the same for all these models
 predict <- matrix(1, nrow = 6, ncol = ncol(mcar10)) %>% 
   set_rownames(paste0("logr", seq(4, 9), "cesd")) %>% 
   set_colnames(colnames(mcar10))
@@ -185,22 +191,26 @@ predict <- matrix(1, nrow = 6, ncol = ncol(mcar10)) %>%
 predict[, c("HHIDPN", "conde", "age_death_y", "r4cesd_elevated", 
             paste0("r", seq(4, 9), "cesd"), "r9cesd_elevated", 
             "total_elevated_cesd", "avg_cesd", "avg_cesd_elevated", 
-            "observed", paste0("r", seq(5, 9), "age_y_int"))] <- 0
+            "observed")] <- 0
 
 # #Use values at the current wave to predict-- not sure about this yet
-# predict[, paste0("r", seq(4, 9), "BMI")] <- diag(x = 1, nrow = 6, ncol = 6)
-# predict[, paste0("r", seq(4, 9), "age_y_int")] <- diag(x = 1, nrow = 6, ncol = 6)
-# predict[, paste0("r", seq(4, 9), "shlt")] <- diag(x = 1, nrow = 6, ncol = 6)
+predict[, paste0("r", seq(4, 9), "BMI")] <- diag(x = 1, nrow = 6, ncol = 6)
+predict[, paste0("r", seq(4, 9), "age_y_int")] <- diag(x = 1, nrow = 6, ncol = 6)
+predict[, paste0("r", seq(4, 9), "shlt")] <- diag(x = 1, nrow = 6, ncol = 6)
 
 #Exclude values that predict themselves
 predict[, paste0("logr", seq(4, 9), "cesd")] <- 
   (diag(x = 1, nrow = 6, ncol = 6) == 0)*1
 
 #---- ***run imputation ----
-jmvn <- mice(data = mcar10, m = num_impute, method = "norm", 
-             predictorMatrix = predict, where = is.na(mcar10), 
-             blocks = as.list(paste0("logr", seq(4, 9), "cesd")), 
-             seed = 20210126)
+for(prop in mask_props){
+  data <- get(paste0("mcar", 100*prop))
+  assign(paste0("jmvn_mcar", 100*prop), 
+         mice(data = data, m = num_impute, method = "norm", 
+              predictorMatrix = predict, where = is.na(data), 
+              blocks = as.list(paste0("logr", seq(4, 9), "cesd")), 
+              seed = 20210126))
+}
 
 #---- ***trace plots ----
 #trace plots-- can plot these in ggplot if we want by accessing chainMean and 
@@ -209,67 +219,8 @@ jmvn <- mice(data = mcar10, m = num_impute, method = "norm",
 png(paste0("/Users/CrystalShaw/Dropbox/Projects/exposure_trajectories/",
            "manuscript/figures/mcar10_jmvn_traceplot.png"), 
     width = 7, height = 4.5, units = "in", res = 300)
-plot(jmvn)
+plot(jmvn10)
 dev.off()
-
-#---- ***visualize imputations ----
-mean_imputation <- vector(mode = "list", length = 6)
-for(i in 1:length(mean_imputation)){
-  wave = i + 3
-  mean_imputation[[i]] = rowMeans(jmvn$imp[[c(paste0("logr", wave, "cesd"))]])
-}
-
-plot_data <- as.data.frame(matrix(nrow = num_missing, ncol = 2)) %>% 
-  set_colnames(c("Observed", "Imputed"))
-plot_data[, "Imputed"] <- unlist(mean_imputation)
-
-observed_data <- 
-  cbind(CESD_data_wide %>% 
-          dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% 
-          pivot_longer(everything(), names_to = "rwave", 
-                       values_to = "complete"), 
-        mcar10 %>% 
-          dplyr::select(paste0("logr", seq(4, 9), "cesd")) %>% 
-          pivot_longer(everything(), names_to = "logrwave", 
-                       values_to = "masked")) %>% 
-  arrange(rwave) %>% filter(is.na(masked))
-
-plot_data[, "Observed"] <- observed_data$complete
-plot_data %<>% mutate("log_observed" = log(1 + Observed), 
-                      "trans_imputed" = round(exp(Imputed) - 1))
-
-# #Sanity check
-# head(CESD_data_wide$r4cesd[as.numeric(names(mean_imputation[[1]]))])
-# head(plot_data)
-# tail(CESD_data_wide$r9cesd[as.numeric(names(mean_imputation[[6]]))])
-# tail(plot_data)
-
-#plot
-ggplot(data = plot_data, aes(x = log_observed, y = Imputed)) + 
-  geom_point(color = "#B4DAE5FF") + 
-  geom_smooth(method = lm, se = FALSE, color = "#F0D77BFF") +
-  geom_abline(slope = 1, intercept = 0, color = "#5C5992FF", lty = "dashed", 
-              size = 1) + labs(x = "Log(1 + Observed)") + 
-  ggtitle(paste0("Missingness Pattern: MCAR 10% \n", 
-                 "Imputation Strategy: Joint Multivariate Normal")) + 
-  theme_minimal()  
-
-ggsave(paste0("/Users/CrystalShaw/Dropbox/Projects/exposure_trajectories/",
-              "manuscript/figures/mcar10_jmvn_obs_pred_log.jpeg"), 
-       device = "jpeg", width = 7, height = 4.5, units = "in", dpi = 300)
-
-ggplot(data = plot_data, aes(x = Observed, y = trans_imputed)) + 
-  geom_point(color = "#B4DAE5FF") + 
-  geom_smooth(method = lm, se = FALSE, color = "#F0D77BFF") +
-  geom_abline(slope = 1, intercept = 0, color = "#5C5992FF", lty = "dashed", 
-              size = 1) + labs(y = "Exp(Imputed) - 1") + 
-  ggtitle(paste0("Missingness Pattern: MCAR 10% \n", 
-                 "Imputation Strategy: Joint Multivariate Normal")) + 
-  theme_minimal()  
-
-ggsave(paste0("/Users/CrystalShaw/Dropbox/Projects/exposure_trajectories/",
-              "manuscript/figures/mcar10_jmvn_obs_pred.jpeg"), 
-       device = "jpeg", width = 7, height = 4.5, units = "in", dpi = 300)
 
 #---- ***effect estimates ----
 #Based on imputations
@@ -353,6 +304,80 @@ pooled_models <- summary(pool(jmvn_model_list))
 pt_ests <- exp(pooled_models$estimate)
 CIs <- exp(cbind(pooled_models$estimate - pooled_models$std.error, 
                  pooled_models$estimate + pooled_models$std.error))
+
+#---- ***visualize imputations ----
+mean_imputation_mcar <- 
+  lapply(mean_imputation_mcar <- vector(mode = "list", length(mask_props)),
+         function(x) x <- lapply(x <- vector(mode = "list", 4),
+                                 function(x) x <- vector(mode = "list", 
+                                                         length = 6)))
+#naming layers of list
+names(mean_imputation_mcar) <- mask_props*100
+for(i in 1:length(mean_imputation_mcar)){
+  names(mean_imputation_mcar[[i]]) <- c("jmvn", "fcs", "jmvn_long", "fcs_long")
+}
+
+for(i in 1:length(mean_imputation_mcar)){
+  data <- get(paste0("jmvn_mcar", mask_props[i]*100))
+  for(j in 1:6){
+    wave = j + 3
+    mean_imputation_mcar[[i]][["jmvn"]][[j]] <- 
+      rowMeans(data$imp[[c(paste0("logr", wave, "cesd"))]])
+  }
+}
+
+plot_data <- as.data.frame(matrix(nrow = num_missing, ncol = 2)) %>% 
+  set_colnames(c("Observed", "Imputed"))
+plot_data[, "Imputed"] <- unlist(mean_imputation)
+
+observed_data <- 
+  cbind(CESD_data_wide %>% 
+          dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% 
+          pivot_longer(everything(), names_to = "rwave", 
+                       values_to = "complete"), 
+        mcar10 %>% 
+          dplyr::select(paste0("logr", seq(4, 9), "cesd")) %>% 
+          pivot_longer(everything(), names_to = "logrwave", 
+                       values_to = "masked")) %>% 
+  arrange(rwave) %>% filter(is.na(masked))
+
+plot_data[, "Observed"] <- observed_data$complete
+plot_data %<>% mutate("log_observed" = log(1 + Observed), 
+                      "trans_imputed" = round(exp(Imputed) - 1))
+
+# #Sanity check
+# head(CESD_data_wide$r4cesd[as.numeric(names(mean_imputation[[1]]))])
+# head(plot_data)
+# tail(CESD_data_wide$r9cesd[as.numeric(names(mean_imputation[[6]]))])
+# tail(plot_data)
+
+#plot
+ggplot(data = plot_data, aes(x = log_observed, y = Imputed)) + 
+  geom_point(color = "#B4DAE5FF") + 
+  geom_smooth(method = lm, se = FALSE, color = "#F0D77BFF") +
+  geom_abline(slope = 1, intercept = 0, color = "#5C5992FF", lty = "dashed", 
+              size = 1) + labs(x = "Log(1 + Observed)") + 
+  ggtitle(paste0("Missingness Pattern: MCAR 10% \n", 
+                 "Imputation Strategy: Joint Multivariate Normal")) + 
+  theme_minimal()  
+
+ggsave(paste0("/Users/CrystalShaw/Dropbox/Projects/exposure_trajectories/",
+              "manuscript/figures/mcar10_jmvn_obs_pred_log.jpeg"), 
+       device = "jpeg", width = 7, height = 4.5, units = "in", dpi = 300)
+
+ggplot(data = plot_data, aes(x = Observed, y = trans_imputed)) + 
+  geom_point(color = "#B4DAE5FF") + 
+  geom_smooth(method = lm, se = FALSE, color = "#F0D77BFF") +
+  geom_abline(slope = 1, intercept = 0, color = "#5C5992FF", lty = "dashed", 
+              size = 1) + labs(y = "Exp(Imputed) - 1") + 
+  ggtitle(paste0("Missingness Pattern: MCAR 10% \n", 
+                 "Imputation Strategy: Joint Multivariate Normal")) + 
+  theme_minimal()  
+
+ggsave(paste0("/Users/CrystalShaw/Dropbox/Projects/exposure_trajectories/",
+              "manuscript/figures/mcar10_jmvn_obs_pred.jpeg"), 
+       device = "jpeg", width = 7, height = 4.5, units = "in", dpi = 300)
+
 
 #---- **FCS ----
 #---- ***predictor matrix ----

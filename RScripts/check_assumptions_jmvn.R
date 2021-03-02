cc1294f371c270556b880c1d604afbe50fc1026c
Crystal Shaw <c.shaw@ucla.edu>
2021-03-02 11:00:32 -0800

    initial commit

---
+++ RScripts/check_assumptions_jmvn.R
@@ -0,0 +1,28 @@
+#---- package loading + options ----
+if (!require("pacman")){
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+}
+
+p_load("here")
+
+#No scientific notation
+options(scipen = 999)
+
+#---- note ----
+# Since the difference between win and OS, put substituted directory here
+# Yingyan's directory: C:/Users/yingyan_wu
+#                      C:/Users/yingyan_wu/Dropbox
+# Crystal's directory: /Users/CrystalShaw
+#                     ~/Dropbox/Projects
+
+#Changing directories here will change them throughout the script
+path_to_box <- "/Users/CrystalShaw"
+path_to_dropbox <- "~/Dropbox/Projects"
+
+#---- read in analytical sample ----
+CESD_data_wide <- 
+  read_csv(paste0(path_to_dropbox, 
+                  "/exposure_trajectories/data/", 
+                  "CESD_data_wide.csv"), 
+           col_types = cols(HHIDPN = col_character())) %>% 
+  mutate_if(is.character, as.factor)

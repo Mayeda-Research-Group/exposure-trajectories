2d95fe809e4b684bcd9e75eacdf8eb229904c5e2
Crystal Shaw <c.shaw@ucla.edu>
2021-03-02 11:27:27 -0800

    check marital status

--- RScripts/check_assumptions_jmvn.R
+++ RScripts/check_assumptions_jmvn.R
@@ -1,3 +1,8 @@
+#This script checks the normality of residuals assumption necessary for FCS with 
+#the "norm" option to be equivalent to Joint Multivariate Normal Modeling
+#
+#Transformations will be made if they help make the assumption more plausible
+
 #---- package loading + options ----
 if (!require("pacman")){
   install.packages("pacman", repos='http://cran.us.r-project.org')
@@ -26,3 +31,34 @@ CESD_data_wide <-
                   "CESD_data_wide.csv"), 
            col_types = cols(HHIDPN = col_character())) %>% 
   mutate_if(is.character, as.factor)
+
+#---- select variables ----
+vars <- c(paste0("r", seq(4, 9), "mstat_impute"), "ed_cat", 
+          paste0("drinking", seq(4, 9), "_impute"), 
+          paste0("r", seq(4, 9), "memrye_impute"),
+          paste0("r", seq(4, 9), "stroke_impute"),
+          paste0("r", seq(4, 9), "hearte_impute"),
+          paste0("r", seq(4, 9), "lunge_impute"),
+          paste0("r", seq(4, 9), "cancre_impute"),
+          paste0("r", seq(4, 9), "hibpe_impute"),
+          paste0("r", seq(4, 9), "diabe_impute"),
+          paste0("r", seq(3, 9), "conde_impute"), "smoker", 
+          paste0("r", seq(4, 9), "BMI"), "hispanic", "white", "black", "other", 
+          "female", paste0("r", seq(4, 9), "age_y_int"), "death2018", 
+          paste0("r", seq(3, 9), "cesd"), paste0("r", seq(4, 9), "shlt"))
+
+model_subset <- CESD_data_wide %>% dplyr::select(all_of(vars))
+
+#---- marital status ----
+for(i in (4:9)){
+  outcome <- paste0("r", i, "mstat_impute")
+  predictors <- 
+    colnames(model_subset[-which(colnames(model_subset) == outcome)])
+  model <- 
+    lm(as.formula(paste0(outcome, "~", paste0(predictors, collapse = "+"))), 
+       data = model_subset)
+  show(hist(model$residuals, main = outcome))
+}
+
+
+

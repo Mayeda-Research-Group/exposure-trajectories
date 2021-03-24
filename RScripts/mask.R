66ae34cc2f9ea55ad9a194ef48bbafbc7626398e
Crystal Shaw <c.shaw@ucla.edu>
2021-03-23 22:06:05 -0700

    remove creation of extra data subsets

--- RScripts/mask.R
+++ RScripts/mask.R
@@ -11,7 +11,7 @@ logit <- function(x){
 
 #---- Mask function ----
 mask <- function(data_wide, mechanism, mask_percent){
-  
+  mask_prop <- as.numeric(sub("%","", mask_percent))/100
   #---- create incomplete data ----
   if(mechanism == "MCAR"){
     #---- MCAR ----
@@ -20,44 +20,46 @@ mask <- function(data_wide, mechanism, mask_percent){
     # consider
     #   Need to add the following: wave-updated marital status, wave-updated 
     #   drinking behavior, wave-updated chronic conditions,
-    mask_prop <- as.numeric(sub("%","", mask_percent))/100
     total_indices <- nrow(data_wide)*6 #6 waves of data per person
     mask_index <- sample(seq(1, total_indices), 
                          size = floor(mask_prop*total_indices), 
                          replace = FALSE)
   } else{
-    
-    mask_prop <- as.numeric(sub("%","", mask_percent))/100
-    
     #---- expected value for predictors of MAR & MNAR missingness ----
-    # Age
-    overall_ages <- data_wide %>% 
-      dplyr::select(paste0("r", seq(4, 9, by = 1), "age_y_int")) %>% 
-      pivot_longer(everything(), names_to = "orig_varname", 
-                   values_to = "age_int")
+    # # Age
+    # overall_ages <- data_wide %>% 
+    #   dplyr::select(paste0("r", seq(4, 9, by = 1), "age_y_int")) %>% 
+    #   pivot_longer(everything(), names_to = "orig_varname", 
+    #                values_to = "age_int")
     
-    # CESD previous wave (wave 3-8)
-    CESD_3_8_long <- data_wide %>%
-      dplyr::select(paste0("r", seq(3, 8, by = 1), "cesd")) %>%
-      pivot_longer(everything(), names_to = "orig_varname", 
-                   values_to = "cesd")
-    
-    # conde at previous wave
-    conde_3_8_long <- data_wide %>%
-      dplyr::select(paste0("r", seq(3, 8, by = 1), "conde_impute")) %>%
-      pivot_longer(everything(), names_to = "orig_varname", 
-                   values_to = "conde_impute")
-    
-    # CESD this wave (wave 4-9)
-    CESD_4_9_long <- data_wide %>%
-      dplyr::select(paste0("r", seq(4, 9, by = 1), "cesd")) %>%
-      pivot_longer(everything(), names_to = "orig_varname", 
-                   values_to = "cesd")
+    # # CESD previous wave (wave 3-8)
+    # CESD_3_8_long <- data_wide %>%
+    #   dplyr::select(paste0("r", seq(3, 8, by = 1), "cesd")) %>%
+    #   pivot_longer(everything(), names_to = "orig_varname", 
+    #                values_to = "cesd")
+    # 
+    # #conde at previous wave
+    # conde_3_8_long <- data_wide %>%
+    #   dplyr::select(paste0("r", seq(3, 8, by = 1), "conde_impute")) %>%
+    #   pivot_longer(everything(), names_to = "orig_varname", 
+    #                values_to = "conde_impute")
+    # 
+    # #CESD this wave (wave 4-9)
+    # CESD_4_9_long <- data_wide %>%
+    #   dplyr::select(paste0("r", seq(4, 9, by = 1), "cesd")) %>%
+    #   pivot_longer(everything(), names_to = "orig_varname", 
+    #                values_to = "cesd")
     #---- **E(X)s ----
-    e_age <- mean(overall_ages$age_int)
-    e_CESD_3_8 <- mean(CESD_3_8_long$cesd, na.rm = T)
-    e_CESD_4_9 <- mean(CESD_4_9_long$cesd)
-    e_conde <- mean(conde_3_8_long$conde_impute, na.rm = T)
+    e_age <- 
+      mean(unlist(data_wide[, paste0("r", seq(4, 9, by = 1), "age_y_int")]))
+    e_CESD_3_8 <- 
+      mean(unlist(data_wide[, paste0("r", seq(3, 8, by = 1), "cesd")]), 
+           na.rm = TRUE)
+    e_CESD_4_9 <- 
+      mean(unlist(data_wide[, paste0("r", seq(4, 9, by = 1), "cesd")]))
+    e_conde <- 
+      mean(unlist(data_wide[, paste0("r", seq(3, 8, by = 1), "conde_impute")]), 
+           na.rm = TRUE)
     
     #---- **betas ----
     # beta_age <- log(1.05)
@@ -74,7 +76,7 @@ mask <- function(data_wide, mechanism, mask_percent){
       beta_0_10 <- logit(0.1) -
         (beta_age_10*e_age + beta_cesdpre_10*e_CESD_3_8 + 
            beta_condepre_10*e_conde)
-
+      
       if (mask_prop == 0.1){
         beta_0 <- beta_0_10
         beta_age <- beta_age_10
@@ -86,7 +88,7 @@ mask <- function(data_wide, mechanism, mask_percent){
         beta_condepre <- beta_condepre_10
         beta_0 <- logit(mask_prop) -
           (beta_age*e_age + beta_cesdpre*e_CESD_3_8 + beta_condepre*e_conde)
-        } else{
+      } else{
         scaling_coef <- logit(mask_prop)/logit(0.1)
         beta_0 <- scaling_coef * beta_0_10
         beta_age <- scaling_coef * beta_age_10
@@ -118,8 +120,8 @@ mask <- function(data_wide, mechanism, mask_percent){
         select(contains("pcesd"))
       
       for (j in 1:ncol(subset)){
-          subset[, paste0("r", j + 3, "cesd_missing")] <- 
-            rbinom(nrow(subset), size = 1, prob = subset[[j]])
+        subset[, paste0("r", j + 3, "cesd_missing")] <- 
+          rbinom(nrow(subset), size = 1, prob = subset[[j]])
       }
       
       subset_long <- subset %>%
@@ -136,7 +138,7 @@ mask <- function(data_wide, mechanism, mask_percent){
       beta_0_10 <- logit(0.1) -
         (beta_age_10*e_age + beta_cesdpre_10*e_CESD_3_8 + 
            beta_condepre_10*e_conde + beta_cesdcurrent_10*e_CESD_4_9)
-
+      
       if (mask_prop == 0.1){
         beta_0 <- beta_0_10
         beta_age <- beta_age_10
@@ -159,7 +161,7 @@ mask <- function(data_wide, mechanism, mask_percent){
         beta_condepre <- scaling_coef * beta_condepre_10
         beta_cesdcurrent <- scaling_coef * beta_cesdcurrent_10
       }
-
+      
       subset <- data_wide %>%
         mutate(
           r4pcesd = expit(beta_0 + beta_age * r4age_y_int + 
@@ -184,8 +186,8 @@ mask <- function(data_wide, mechanism, mask_percent){
         select(contains("pcesd"))
       
       for (j in 1:ncol(subset)){
-          subset[, paste0("r", j + 3, "cesd_missing")] <- 
-            rbinom(nrow(subset), size = 1, prob = subset[[j]])
+        subset[, paste0("r", j + 3, "cesd_missing")] <- 
+          rbinom(nrow(subset), size = 1, prob = subset[[j]])
         
       }
       
@@ -217,13 +219,13 @@ mask <- function(data_wide, mechanism, mask_percent){
     #need to get rid of rows with NA values to pivot back to wide
     data_long %<>% na.omit() %>%
       pivot_wider(names_from = "name", values_from = "value")
-
+    
     #mask in wide dataset with masked values
     data_wide[which(data_wide$HHIDPN %in% data_long$HHIDPN),
               paste0("r", seq(4, 9), var)] <- data_long[, -1]
     data_wide[which(!data_wide$HHIDPN %in% data_long$HHIDPN),
               paste0("r", seq(4, 9), var)] <- NA
-
+    
   }
   
   #masking derived values
@@ -238,6 +240,7 @@ mask <- function(data_wide, mechanism, mask_percent){
              rowMeans(data_wide %>% 
                         dplyr::select(paste0("r", seq(4, 9), "cesd"))), 
            "avg_cesd_elevated" = ifelse(avg_cesd > 4, 1, 0))
+  
   #---- check missings ----
   # make sure no one is missing every cesd measure
   data_wide %<>%
@@ -304,4 +307,3 @@ mask <- function(data_wide, mechanism, mask_percent){
 #   select(contains("r5")) %>%
 #   filter(is.na(r5cesd))
 
-  

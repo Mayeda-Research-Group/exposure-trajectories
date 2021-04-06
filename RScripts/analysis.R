e0ee4c97718e0ea9a0f29be55f59b8a86e2b5d44
Crystal Shaw <c.shaw@ucla.edu>
2021-04-06 10:38:49 -0700

    change path

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -24,7 +24,7 @@ source(here::here("RScripts", "mask_impute_pool.R"))
 # MRG desktop directory: C:/Users/cshaw/Dropbox
 
 #Changing directories here will change them throughout the script
-path_to_dropbox <- "C:/Users/cshaw/Dropbox/Projects"
+path_to_dropbox <- "~/Dropbox/Projects"
 
 #---- read in analytical sample ----
 CESD_data_wide <- 
@@ -43,10 +43,10 @@ exposures <- c("CES-D Wave 4", "CES-D Wave 9", "Elevated Average CES-D",
                "Elevated CES-D Count")
 #all methods: "JMVN", "FCS", "PMM", "JMVN Long", "FCS Long"
 methods <- c("FCS")
-#mechanisms <- c("MCAR")
-#mask_props <- c(0.10)
-mechanisms <- c("MCAR", "MAR", "MNAR")
-mask_props <- c(.10, 0.20, 0.30)
+mechanisms <- c("MCAR")
+mask_props <- c(0.10)
+#mechanisms <- c("MCAR", "MAR", "MNAR")
+#mask_props <- c(.10, 0.20, 0.30)
 
 table_effect_ests <- 
   data.frame(expand_grid(exposures, "Truth", mechanisms, "0%")) %>% 

948b2b116da47895f4b6aef75e8ffc8be587b4ba
Crystal <c.shaw@ucla.edu>
2021-04-06 11:30:50 -0700

    fixed MRG path... again

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -21,10 +21,10 @@ source(here::here("RScripts", "mask_impute_pool.R"))
 #                      C:/Users/yingyan_wu/Dropbox
 # Crystal's directory: /Users/CrystalShaw
 #                     ~/Dropbox/Projects
-# MRG desktop directory: C:/Users/cshaw/Dropbox
+# MRG desktop directory: C:/Users/cshaw/Dropbox/Projects
 
 #Changing directories here will change them throughout the script
-path_to_dropbox <- "~/Dropbox/Projects"
+path_to_dropbox <- "C:/Users/cshaw/Dropbox/Projects"
 
 #---- read in analytical sample ----
 CESD_data_wide <- 
@@ -43,10 +43,10 @@ exposures <- c("CES-D Wave 4", "CES-D Wave 9", "Elevated Average CES-D",
                "Elevated CES-D Count")
 #all methods: "JMVN", "FCS", "PMM", "JMVN Long", "FCS Long"
 methods <- c("FCS")
-mechanisms <- c("MCAR")
-mask_props <- c(0.10)
-#mechanisms <- c("MCAR", "MAR", "MNAR")
-#mask_props <- c(.10, 0.20, 0.30)
+#mechanisms <- c("MCAR")
+#mask_props <- c(0.10)
+mechanisms <- c("MCAR", "MAR", "MNAR")
+mask_props <- c(.10, 0.20, 0.30)
 
 table_effect_ests <- 
   data.frame(expand_grid(exposures, "Truth", mechanisms, "0%")) %>% 

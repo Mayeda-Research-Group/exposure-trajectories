9aa8f6353cc55bc16180345da29b299b4d4b9831
Crystal Shaw <c.shaw@ucla.edu>
2021-04-20 10:15:52 -0700

    try short run to replicate error

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -40,17 +40,17 @@ CESD_data_wide <-
 
 #---- Table 2 shell: Effect Estimates ----
 #Number of simulation runs
-num_runs <- 10
+num_runs <- 2
 exposures <- c("CES-D Wave 4", "CES-D Wave 9", "Elevated Average CES-D", 
                "Elevated CES-D Count")
 #all methods: "JMVN", "FCS", "PMM", "LMM"
-#methods <- c("LMM")
-#mechanisms <- c("MCAR")
-#mask_props <- c(0.10)
+methods <- c("PMM")
+mechanisms <- c("MCAR")
+mask_props <- c(0.10)
 
-methods <- c("JMVN", "PMM", "FCS")
-mechanisms <- c("MCAR", "MAR", "MNAR")
-mask_props <- c(.10, 0.20, 0.30)
+#methods <- c("JMVN", "PMM", "FCS")
+# mechanisms <- c("MCAR", "MAR", "MNAR")
+# mask_props <- c(.10, 0.20, 0.30)
 
 table_effect_ests <- 
   data.frame(expand_grid(exposures, "Truth", mechanisms, "0%")) %>% 

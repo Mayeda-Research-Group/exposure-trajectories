944c63a65331db34d4145be99058f9a345845f41
Crystal Shaw <c.shaw@ucla.edu>
2021-05-21 11:17:11 -0700

    create interaction term in dataset

--- RScripts/Preliminary analysis/MAR_MNAR_betas_deep_dive.R
+++ RScripts/Preliminary analysis/MAR_MNAR_betas_deep_dive.R
@@ -62,6 +62,12 @@ data_wide <-
                   "/exposure_trajectories/data/", 
                   "CESD_data_wide.csv"))
 
+for(wave in seq(4, 9)){
+  data_wide %<>% 
+    mutate(!!paste0("r", wave, "cesd_death2018") := 
+             !!sym(paste0("r", wave, "cesd"))*death2018)
+}
+
 #---- expected value for predictors of MAR & MNAR missingness ----
 #---- **E(X)s ----
 e_age <- 
@@ -77,6 +83,9 @@ e_conde <-
 e_shlt <- mean(unlist(data_wide[, paste0("r", seq(3, 8, by = 1), "shlt")]), 
                na.rm = TRUE)
 e_death2018 <- mean(data_wide$death2018)
+e_death2018_CESD_4_9 <- 
+  mean(unlist(data_wide[, paste0("r", seq(4, 9, by = 1), "")]), 
+                             na.rm = TRUE)
 
 # #---- ** MAR Coefficients ----
 # #beta_age <- log(0.97*scale)
@@ -92,84 +101,36 @@ e_death2018 <- mean(data_wide$death2018)
 #beta_shltpre <- log(1.100)
 beta_death2018 <- log(1.5)
 beta_cesdcurrent <- log(4)
-beta_death2018*cesdcurrent <- log(1.5)
+beta_death2018_cesdcurrent <- log(1.5)
 
-#----  scale coef function ----
-coef_func <- function (dataset, mechanism){
+#----  coef function ----
+coef_func <- function (dataset, mechanism, mask_prop){
   
   n <- nrow(dataset)
-  beta_0 <- -6
-  #beta_age_s <- beta_age * scale
-  beta_cesdpre_s <- beta_cesdpre * scale
-  beta_condepre_s <- beta_condepre * scale
-  #beta_shltpre_s <- beta_shltpre * scale
-  beta_death2018_s <- beta_death2018 * scale
-  beta_cesdcurrent_s <- beta_cesdcurrent * scale
   
-  #  # balancing intercept (TEMP)
-  #  #---- **E(X)s ----
-  #  e_age <- 
-  #    mean(unlist(dataset[, paste0("r", seq(4, 9, by = 1), "age_y_int")]))
-  #  e_CESD_3_8 <- 
-  #    mean(unlist(dataset[, paste0("r", seq(3, 8, by = 1), "cesd")]), 
-  #         na.rm = TRUE)
-  #  e_CESD_4_9 <- 
-  #    mean(unlist(dataset[, paste0("r", seq(4, 9, by = 1), "cesd")]))
-  #  e_conde <- 
-  #    mean(unlist(dataset[, paste0("r", seq(3, 8, by = 1), "conde_impute")]), 
-  #         na.rm = TRUE)
-  #  e_shlt <- mean(unlist(dataset[, paste0("r", seq(3, 8, by = 1), "shlt")]), 
-  #                 na.rm = TRUE)
-  #  e_death2018 <- mean(dataset$death2018)
-  #  
-  # ( beta_0_MAR <- logit(0.40) - (
-  #    beta_age_s*e_age + beta_cesdpre_s*e_CESD_3_8 + beta_condepre_s*e_conde + 
-  #      beta_shltpre_s*e_shlt + beta_death2018_s*e_death2018))
-  #  exp(beta_0_MAR_10)
-  
-  if (mechanism == "MAR"){
-    #---- MAR ----
-    subset <- dataset %>%
-      mutate( 
-        r4pcesd_MAR = expit(#beta_age_s * r4age_y_int + 
-          beta_cesdpre_s * r3cesd +
-            beta_condepre_s * r3conde_impute +
-            #beta_shltpre_s * r3shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0),
-        r5pcesd_MAR = expit(#beta_age_s * r5age_y_int + 
-          beta_cesdpre_s * r4cesd +
-            beta_condepre_s * r4conde_impute +
-            #beta_shltpre_s * r4shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0),
-        r6pcesd_MAR = expit(#beta_age_s * r6age_y_int + 
-          beta_cesdpre_s * r5cesd +
-            beta_condepre_s * r5conde_impute +
-            #beta_shltpre_s * r5shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0),
-        r7pcesd_MAR = expit(#beta_age_s * r7age_y_int + 
-          beta_cesdpre_s * r6cesd +
-            beta_condepre_s * r6conde_impute +
-            #beta_shltpre_s * r6shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0),
-        r8pcesd_MAR = expit(#beta_age_s * r8age_y_int + 
-          beta_cesdpre_s * r7cesd +
-            beta_condepre_s * r7conde_impute +
-            #beta_shltpre_s * r7shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0),
-        r9pcesd_MAR = expit(#beta_age_s * r9age_y_int + 
-          beta_cesdpre_s * r8cesd +
-            beta_condepre_s * r8conde_impute +
-            #beta_shltpre_s * r8shlt + 
-            beta_death2018_s * death2018 + 
-            beta_0)) %>%
-      dplyr::select(contains("MAR", ignore.case = FALSE))
-  } else if(mechanism == "MNAR"){
+  if (mechanism == "MNAR"){
     #---- MNAR ----
+    #balancing intercept
+    beta_0 <- logit(mask_prop) - 
+      (beta_death2018*e_death2018 + beta_cesdcurrent*e_CESD_4_9 + 
+         beta_death2018_cesdcurrent*e_CESD_4_9*e_death2018)
+    
+    subset <- dataset
+    
+    for(wave in seq(4, 9)){
+      subset %<>% 
+        mutate(!!paste0("r", wave, "pcesd") := 
+                 expit(beta_death2018*death2018 + 
+                         beta_cesdcurrent*
+                         !!sym(paste0("r", wave, "cesd")) + 
+                         beta_death2018_cesdcurrent*
+                         death2018*!!sym(paste0("r", wave, "cesd")) + beta_0))
+    }
+    
+    subset %<>% dplyr::select(contains("pcesd", ignore.case = FALSE))
+    
+  } else if(mechanism == "MAR"){
+    #---- MAR ----
     subset <- dataset %>%
       mutate( 
         r4pcesd_MNAR = expit(#beta_age_s * r4age_y_int + 
@@ -220,17 +181,10 @@ coef_func <- function (dataset, mechanism){
   # Calculate the missing proportion
   subset_long <- subset %>%
     select(contains("cesd_missing")) %>%
-    pivot_longer(
-      everything(),
-      names_to = "Variables",
-      values_to = "Missingness"
-    )
+    pivot_longer(everything(), names_to = "Variables", values_to = "Missingness")
   
-  return(
-    Missing_prop_results <- tibble(
-      missing_prop = mean(subset_long$Missingness, na.rm = T)
-    )
-  )
+  return(Missing_prop_results <- 
+           tibble(missing_prop = mean(subset_long$Missingness, na.rm = T)))
 }
 
 # Repeat for 1000 times
@@ -250,7 +204,8 @@ missing_prop_MAR %>%
 
 #---- run MNAR sim ----
 {missing_prop_MNAR <- 
-  map_dfr(1:replicate, ~ scale_coef_func(data_wide, "MNAR", 1),
+  map_dfr(1:replicate, ~ 
+            coef_func(dataset = data_wide, mechanism = "MNAR", mask_prop = 0.20), 
           .id = "replication") %>% estimate_df()
 
 missing_prop_MNAR %>%

2c18485f5bbf5ff09627cc30e85d3f3a3c2286bf
Crystal Shaw <c.shaw@ucla.edu>
2021-05-24 11:18:39 -0700

    save optimized beta0 for MNAR

--- RScripts/Preliminary analysis/MAR_MNAR_beta_0_optimizer.R
+++ RScripts/Preliminary analysis/MAR_MNAR_beta_0_optimizer.R
@@ -188,12 +188,20 @@ set.seed(20210507)
 
 {missing_prop_MNAR <- 
     map_dfr(1:replicate, ~ test_mask(dataset = data_wide, mechanism = "MNAR", 
-                                     beta_0 = optim_MNAR10$minimum), 
+                                     beta_0 = optim_MNAR30$minimum), 
             .id = "replication") %>% estimate_df()
   
   missing_prop_MNAR %>%
     kbl(caption = "MNAR missingness")%>%
     kable_classic(full_width = F, html_font = "Arial")
+}
+
+#---- **save optimized beta_0 ----
+for(percent in c(10, 20, 30)){
+  write_rds(get(paste0("optim_MNAR", percent)), 
+            file = paste0(path_to_dropbox, "/exposure_trajectories/data/", 
+                          "optimized_masking_intercepts/optim_MNAR", percent, 
+                          ".RDS"))
 }
 
 #---- run MAR sim ----

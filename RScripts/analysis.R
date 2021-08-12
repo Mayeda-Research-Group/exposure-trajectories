d7f385ba71b5f9aed24491b620693892a2f76131
Crystal <c.shaw@ucla.edu>
2021-08-11 18:01:06 -0700

    update imputation scenario

--- RScripts/analysis.R
+++ RScripts/analysis.R
@@ -88,7 +88,7 @@ exposures <- c("CES-D Wave 4", "CES-D Wave 9", "Elevated Average CES-D",
                "Elevated CES-D Count")
 
 #all methods: "JMVN", "FCS", "PMM", "LMM"
-methods <- c("PMM")
+methods <- c("JMVN")
 mechanisms <- c("MNAR")
 mask_props <- c(0.10, 0.20, 0.30)
 

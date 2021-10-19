37bae923c68c600215a98333733b8543ca881bab
cshawsome <c.shaw@ucla.edu>
2021-10-19 10:48:34 -0700

    remove unecessary step in CC analysis

--- RScripts/mask_impute_pool.R
+++ RScripts/mask_impute_pool.R
@@ -281,8 +281,7 @@ mask_impute_pool <-
       
       #indicate where to take averages
       indicator <- complete_data %>% 
-        dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% 
-        mutate_all(function(x) ifelse(x > 4, 1, 0)) %>% is.na() %>% 
+        dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% is.na() %>% 
         rowSums() > 2
       complete_data %<>% mutate("avg_indicator" = 1 - (1*indicator))
       
@@ -519,3 +518,80 @@ mask_impute_pool <-
 mask_impute_pool(mechanism = mechanism, method = method, 
                  mask_percent = mask_percent, directory = "/u/home/c/cshaw343/", 
                  save = "no")
+
+#---- test CC analysis ----
+#---- **mask data ----
+test_cc_analysis <- function(data, mechanism, mask_percent){
+  complete_data <- 
+    mask(data, mechanism, mask_percent, beta_0_table, beta_mat)
+  
+  complete_data %<>% 
+    mutate("r4cesd_elevated" = ifelse(r4cesd > 4, 1, 0), 
+           "r9cesd_elevated" = ifelse(r9cesd > 4, 1, 0))
+  
+  #indicate where to take averages
+  indicator <- complete_data %>% 
+    dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% is.na() %>% 
+    rowSums() > 2
+  complete_data %<>% mutate("avg_indicator" = 1 - (1*indicator))
+  
+  complete_data[, "avg_cesd"] <- 
+    rowMeans(complete_data %>% 
+               dplyr::select(paste0("r", seq(4, 9), "cesd")), na.rm = TRUE) 
+  complete_data[which(complete_data$avg_indicator == 0), "avg_cesd"] <- NA
+  complete_data %<>% 
+    mutate("avg_cesd_elevated" = ifelse(avg_cesd > 4, 1, 0))
+  
+  complete_data[, "prop_elevated_cesd"] <- 
+    rowMeans(complete_data %>% 
+               dplyr::select(paste0("r", seq(4, 9), "cesd")) %>% 
+               mutate_all(function(x) ifelse(x > 4, 1, 0)), na.rm = TRUE) 
+  complete_data[which(complete_data$avg_indicator == 0), 
+                "prop_elevated_cesd"] <- NA
+  
+  #---- ****models ----
+  model_list <- vector(mode = "list", length = length(exposures)) %>% 
+    set_names(exposures)
+  
+  for(exposure in exposures){
+    if(exposure == "CES-D Wave 4"){
+      model_list[[exposure]] <- 
+        with(complete_data, 
+             coxph(Surv(survtime, observed) ~ r4not_married_partnered + 
+                     r4widowed + ed_cat + r4drinking_cat + r4memrye_impute + 
+                     r4stroke_impute + r4hearte_impute + r4lunge_impute + 
+                     r4cancre_impute + r4hibpe_impute + r4diabe_impute + 
+                     smoker + r4BMI + hispanic + black + other + female + 
+                     r4age_y_int + r4cesd_elevated))
+    } else if(exposure == "CES-D Wave 9"){
+      model_list[[exposure]] <- 
+        with(complete_data, 
+             coxph(Surv(survtime, observed) ~ r9not_married_partnered + 
+                     r9widowed + ed_cat + r9drinking_cat + r9memrye_impute + 
+                     r9stroke_impute + r9hearte_impute + r9lunge_impute + 
+                     r9cancre_impute + r9hibpe_impute + r9diabe_impute + 
+                     smoker + r9BMI + hispanic + black + other + female + 
+                     r9age_y_int + r9cesd_elevated))
+    } else if(exposure == "Elevated CES-D Count"){
+      model_list[[exposure]] <- 
+        with(complete_data, 
+             coxph(Surv(survtime, observed) ~ r4not_married_partnered + 
+                     r4widowed + ed_cat + r4drinking_cat + r4memrye_impute + 
+                     r4stroke_impute + r4hearte_impute + r4lunge_impute + 
+                     r4cancre_impute + r4hibpe_impute + r4diabe_impute + 
+                     smoker + r4BMI + hispanic + black + other + female + 
+                     r4age_y_int + total_elevated_cesd))
+    } else if(exposure == "Elevated CES-D Prop"){
+      model_list[[exposure]] <- 
+        with(complete_data, 
+             coxph(Surv(survtime, observed) ~ r4not_married_partnered + 
+                     r4widowed + ed_cat + r4drinking_cat + r4memrye_impute + 
+                     r4stroke_impute + r4hearte_impute + r4lunge_impute + 
+                     r4cancre_impute + r4hibpe_impute + r4diabe_impute + 
+                     smoker + r4BMI + hispanic + black + other + female + 
+                     r4age_y_int + prop_elevated_cesd))
+      
+    }
+  }
+}
+

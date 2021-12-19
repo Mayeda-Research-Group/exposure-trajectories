07532fe7e6bc72364ad591fdbb5900d1051dab37
cshawsome <c.shaw@ucla.edu>
2021-12-18 17:01:42 -0800

    update resources requested update number of runs

--- submission_scripts/submit_PMM_missing_MNAR30_runs.sh
+++ submission_scripts/submit_PMM_missing_MNAR30_runs.sh
@@ -3,21 +3,21 @@
 # error = Merged with joblog
 #$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
 #$ -j y 
-#$ -l h_rt=5:00:00,h_data=3G #requests 5 hours, 3GB of data (per core)
-#$ -pe shared 6 #requests 6 cores
+#$ -l h_rt=5:00:00,h_data=2G #requests 5 hours, 2GB of data (per core)
+#$ -pe shared 1 #requests 1 core
 # Email address to notify
 #$ -M $USER@mail #don't change this line, finds your email in the system 
 # Notify when
 #$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
 # submit array job:
 # FULL RUN:
-#$ -t 1-116:1
+#$ -t 1-40:1
 ## 
 
 # load the job environment:
 . /u/local/Modules/default/init/modules.sh
 module load R/4.0.2 #loads R/4.0.2 for use 
-export OMP_NUM_THREADS=6 #uses max 6 threads (needs to match -pe shared)
+export OMP_NUM_THREADS=1 #uses max 1 thread (needs to match -pe shared)
 ## 
 # run R code
 

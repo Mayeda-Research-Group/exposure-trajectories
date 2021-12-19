007344112c944aa3ff4f438d8e2ce05ae8f55f93
cshawsome <c.shaw@ucla.edu>
2021-12-18 17:02:26 -0800

    typos in comments

--- submission_scripts/submit_PMM_missing_MAR30_runs.sh
+++ submission_scripts/submit_PMM_missing_MAR30_runs.sh
@@ -4,7 +4,7 @@
 #$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
 #$ -j y 
 #$ -l h_rt=5:00:00,h_data=2G #requests 5 hours, 2GB of data (per core)
-#$ -pe shared 1 #requests 1 cores
+#$ -pe shared 1 #requests 1 core
 # Email address to notify
 #$ -M $USER@mail #don't change this line, finds your email in the system 
 # Notify when
@@ -17,7 +17,7 @@
 # load the job environment:
 . /u/local/Modules/default/init/modules.sh
 module load R/4.0.2 #loads R/4.0.2 for use 
-export OMP_NUM_THREADS=1 #uses max 6 threads (needs to match -pe shared)
+export OMP_NUM_THREADS=1 #uses max 1 thread (needs to match -pe shared)
 ## 
 # run R code
 

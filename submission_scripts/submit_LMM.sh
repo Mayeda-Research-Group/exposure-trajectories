ed0617c86aed6ccabe43e91210c0dc5aa15b2ae2
cshawsome <c.shaw@ucla.edu>
2022-01-24 15:37:08 -0800

    test seed setting

--- submission_scripts/submit_LMM.sh
+++ submission_scripts/submit_LMM.sh
@@ -3,7 +3,7 @@
 # error = Merged with joblog
 #$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
 #$ -j y 
-#$ -l highp,h_rt=30:00:00,h_data=4G #requests 30 hours, 4GB of data (per core) on biostat high performance nodes
+#$ -l highp,h_rt=36:00:00,h_data=4G #requests 36 hours, 4GB of data (per core) on biostat high performance nodes
 #$ -pe shared 1 #requests 1 core
 # Email address to notify
 #$ -M $USER@mail #don't change this line, finds your email in the system 
@@ -11,9 +11,9 @@
 #$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
 # submit array job:
 # TO TEST MATTER RUN ONLY TWICE:
-##$ -t 1-2:1
+#$ -t 1-2:1
 # FOR THE FULL RUN USE INSTEAD:
-#$ -t 1-998:1
+##$ -t 1-998:1
 ## 
 
 # load the job environment:
@@ -31,6 +31,6 @@ export OMP_NUM_THREADS=1 #uses max 1 thread (needs to match -pe shared)
 
 echo "======"
 echo SGE_TASK_ID=$SGE_TASK_ID      
-R CMD BATCH --no-save --no-restore '--args mechanism="MAR" method="LMM" mask_percent="30%" save="no" sens="no" '  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
-echo R CMD BATCH --no-save --no-restore \'--args mechanism=\"MAR\" method=\"LMM\" mask_percent=\"30%\" save=\"no\" sens=\"no\" \'  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
+R CMD BATCH --no-save --no-restore "--args mechanism=\"MAR\" method=\"LMM\" mask_percent=\"30%\" seed=$SGE_TASK_ID save=\"no\" sens=\"no\" "  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
+echo R CMD BATCH --no-save --no-restore \'--args mechanism=\"MAR\" method=\"LMM\" mask_percent=\"30%\" seed=$SGE_TASK_ID save=\"no\" sens=\"no\" \'  mask_impute_pool.R output/output.$JOB_ID.$SGE_TASK_ID
 

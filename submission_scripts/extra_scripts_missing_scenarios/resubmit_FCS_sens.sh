ef3449b57ea1f0b09005e1169f4bc96b365ae0c8
cshawsome <c.shaw@ucla.edu>
2022-03-30 11:50:53 -0700

    these don't work =(

--- submission_scripts/extra_scripts_missing_scenarios/resubmit_FCS_sens.sh
+++ submission_scripts/extra_scripts_missing_scenarios/resubmit_FCS_sens.sh
@@ -1,5 +1,6 @@
 ################################################################################
-### this submission script was created on JAN2022
+### this submission script was created on March2022
+### Use this to resubmit jobs that didn't complete
 ### with ref: https://support.idre.ucla.edu/helpdesk/Ticket/40244340
 ### author RD for inquiries: hpc@ucla.edu
 ### it submits an array job of 9000 tasks for:
@@ -24,7 +25,7 @@
 # TEST OUT BY RUNNING ONLY 9 CASES:
 ##$ -t 1-9:1
 # FOR THE FULL RUN USE INSTEAD (9*num runs):
-#$ -t 1-9000:1
+#$ -t 1-100:1
 ## 
 
 # load the job environment:
@@ -46,7 +47,15 @@ echo "======"
 echo "TASK $SGE_TASK_ID of JOB $JOB_ID started on " `hostname`
 echo "TASK $SGE_TASK_ID of JOB $JOB_ID started at " `date`
 
-myindex=$((SGE_TASK_ID%9))
+# SET THE ARRAY WITH THE $SGE_TASK_IDs that failed 
+### (note: UPDATE line "#$ -t 1-9000:1" ACCORDINGLY!!!) 
+
+array=(76 121 344 439 508 688 808 841 1351 1384 1705 1744 1750 2068 2153 2260 2323 2632 2649 2710 2917 2920 2941 2992 3142 3178 3208 3469 3658 3682 3733 3778 3781 3793 3838 3874 3877 3883 4090 4123 4147 4270 4317 4345 4435 4522 4537 4897 4936 4978 5077 5095 5190 5248 5259 5286 5365 5397 5692 5701 5767 5791 5807 5869 5923 5998 6016 6088 6118 6166 6208 6496 6598 6619 6670 6751 6760 6791 6832 6847 6967 7043 7069 7078 7105 7147 7183 7516 7549 7592 7630 7636 7969 8056 8131 8188 8230 8383 8572 8575)
+
+# SET THE SEED
+###(note: array index starts at 0 so we need to scale back the current $SGE_TASK_ID)
+myindex=${array[$SGE_TASK_ID-1]}
+
 mylastindex=$((myindex%3))
 if [ $myindex == 0 ]; then
   myindex=9

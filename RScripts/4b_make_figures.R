011813bc08193e83f5d8dc558cfe6263e5080d9d
cshawsome <c.shaw@ucla.edu>
2022-04-14 13:52:04 -0700

    delete seminar presentation code

--- RScripts/4b_make_figures.R
+++ RScripts/4b_make_figures.R
@@ -298,31 +298,6 @@ ggsave(paste0(path_to_dropbox, "/exposure_trajectories/",
               "manuscript/figures/figure2/effect_ests_mean_CI.jpeg"), 
        device = "jpeg", dpi = 300, width = 9, height = 7, units = "in")
 
-#---- **for seminar presentation (delete later) ----
-ggplot(results_summary %>% 
-         filter(Exposure %in% c("Elevated Baseline CES-D", 
-                                "Elevated Average CES-D")), 
-       aes(x = Beta, y = Percent, color = Method, shape = Method)) +
-  geom_point(size = 2.0, position = position_dodge(-0.8)) + 
-  scale_shape_manual(values = c(rep("square", (nrow(results_summary))))) + 
-  geom_errorbar(aes(xmin = LCI, xmax = UCI), width = .3,
-                position = position_dodge(-0.8)) +
-  theme_minimal() + ylab("Percent Missing Data") +
-  theme(legend.position = "bottom", legend.direction = "horizontal") + 
-  scale_color_manual(values = cbPalette) + 
-  scale_y_discrete(limits = rev(levels(results_summary$Percent))) + 
-  geom_vline(xintercept = 0, linetype = "dashed", color = "dark grey") + 
-  facet_grid(rows = vars(Mechanism), cols = vars(Exposure)) + 
-  geom_vline(data = truth %>% 
-               filter(Exposure %in% c("Elevated Baseline CES-D", 
-                                      "Elevated Average CES-D")), 
-             aes(xintercept = Beta)) + 
-  theme(text = element_text(size = 13))  
-
-ggsave(paste0("~/Dropbox/UCLA/Classes/BIOSTAT 246/Spring 2022/figures/",
-              "effect_ests_mean_CI.jpeg"), 
-       device = "jpeg", dpi = 300, width = 6, height = 7, units = "in")
-
 #---- Figure 3: RMSE ----
 #---- **read in data ----
 rmse_table <- read_csv(paste0(path_to_dropbox, "/exposure_trajectories/",

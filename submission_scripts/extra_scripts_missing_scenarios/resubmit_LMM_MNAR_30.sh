d2f15361e6547e3d19688edb84fd2226279560c0
cshawsome <c.shaw@ucla.edu>
2022-07-13 12:44:36 -0700

    resubmit jobs

--- submission_scripts/extra_scripts_missing_scenarios/resubmit_LMM_MNAR_30.sh
+++ submission_scripts/extra_scripts_missing_scenarios/resubmit_LMM_MNAR_30.sh
@@ -3,7 +3,7 @@
 # error = Merged with joblog
 #$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
 #$ -j y 
-#$ -l highp,h_rt=60:00:00,h_data=4G #requests 60 hours, 4GB of data (per core) on biostat high performance nodes
+#$ -l highp,h_rt=75:00:00,h_data=4G #requests 75 hours, 4GB of data (per core) on biostat high performance nodes
 #$ -pe shared 1 #requests 1 core
 # Email address to notify
 #$ -M $USER@mail #don't change this line, finds your email in the system 
@@ -13,7 +13,7 @@
 # TO TEST MATTER RUN ONLY TWICE:
 ##$ -t 1-2:1
 # FOR THE FULL RUN USE INSTEAD (length of the array of missing jobs):
-#$ -t 1-28:1
+#$ -t 1-6:1
 ## 
 
 # load the job environment:
@@ -35,7 +35,7 @@ echo SGE_TASK_ID=$SGE_TASK_ID
 # SET THE ARRAY WITH THE $SGE_TASK_IDs that failed 
 ### (note: UPDATE line "#$ -t 1-1000:1" ACCORDINGLY!!!) 
 
-array=(949 950 951 952 975 978 979 980 981 982 983 984 985 986 987 988 989 990 991 992 993 994 995 996 997 998 999 1000)
+array=(990 991 993 995 996 998)
 
 # SET THE SEED
 ###(note: array index starts at 0 so we need to scale back the current $SGE_TASK_ID)
